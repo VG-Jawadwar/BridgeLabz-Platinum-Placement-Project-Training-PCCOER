b2d3a387b8bd56e5a6831adf2621f1e7c16c82af
VG-Jawadwar <vaibhavjawadwar5@gmail.com>
2026-08-09 21:54:08 +0530

    Adding Day 2 Code Files

---
+++ Day 2/bl_day2.java
@@ -0,0 +1,295 @@
+package bridgeLabz;
+
+import java.util.Scanner;
+
+public class bl_day2{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//program to accept info from user:
+		
+//		int roll;
+//		double GPA;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Student Id: ");
+//		roll = sc.nextInt();
+//		System.out.println("Enter Student GPA: ");
+//		GPA = sc.nextDouble();
+//		System.out.println("Student Id: "+roll);
+//		System.out.println("Student Id: "+GPA);
+		
+		
+		//Swapping of 2 numbers Using 3rd Variable:
+		
+//		int num1, num2, temp;
+//		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Num1: ");
+//		num1 = sc.nextInt();
+//		System.out.println("Enter Num2: ");
+//		num2 = sc.nextInt();
+//		
+//		System.out.println("Before: "+num1+" "+num2);
+//		temp = num1; 
+//		num1 = num2;
+//		num2 = temp;
+//		System.out.println("\nAfter: "+num1+" "+num2);
+		
+		
+		//Swapping of 2 numbers Without Using 3rd Variable:
+		
+//		int num1, num2 ;
+//	
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Num1: ");
+//		num1 = sc.nextInt();
+//		System.out.println("Enter Num2: ");
+//		num2 = sc.nextInt();
+//		
+//		System.out.println("Before: "+num1+" "+num2);
+//		num2 = num1 + num2; 
+//		num1 = num2 - num1;
+//		num2 = num2 - num1;
+//		System.out.println("\nAfter: "+num1+" "+num2);
+		
+		
+		// Type Casting
+		
+//		int num1, num2 ;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Num1: ");
+//		num1 = sc.nextInt();
+//		System.out.println("Enter Num2: ");
+//		num2 = sc.nextInt();
+//		
+//		double result =(double) num1/num2;
+//		
+//		System.out.println("Result: "+result);
+		
+		
+		
+		//relational and logical operators
+//		int num1, num2 ;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Num1: ");
+//		num1 = sc.nextInt();
+//		System.out.println("Enter Num2: ");
+//		num2 = sc.nextInt();
+//		
+//		if(num1 > num2 && num1 != 0)
+//		{
+//			System.out.println("num1 is Greater");
+//		}else
+//		{
+//			System.out.println("num2 is Greater");
+//		}
+		
+		
+		//Causes Compile time error as if statement can only accept and process boolean satements i.e true or false
+//		if(System.out.println("Welcome"))
+//		{
+//			
+//		}
+		
+		
+		// Operators
+		
+//		int num1, num2 ;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Num1: ");
+//		num1 = sc.nextInt();
+//		System.out.println("Enter Num2: ");
+//		num2 = sc.nextInt();
+//		
+//		System.out.println("Addition: "+(num1+num2));
+//		System.out.println("Substraction: "+(num1-num2));
+//		System.out.println("Multiplication: "+(num1*num2));
+//		System.out.println("Modulus: "+(num1%num2));
+		
+//		int num1, num2,num3 ;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Num1: ");
+//		num1 = sc.nextInt();
+//		System.out.println("Enter Num2: ");
+//		num2 = sc.nextInt();
+//		System.out.println("Enter Num3: ");
+//		num3 = sc.nextInt();
+//		
+//		if(num1 > 0 && num2>0&&num3>0)
+//		{
+//			System.out.println("All numbers are positive");
+//		}
+//		
+//		if((num1%2==0) ||(num2%2==0) ||(num3%2==0))
+//		{
+//			System.out.println("At Least one Number is Even");
+//		}
+//			
+
+		//Leap Year
+		
+//		int year;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Year: ");
+//		year = sc.nextInt();
+//		
+//		if((year%4 == 0) || (year%100 == 0 && year%400 == 0))
+//		{
+//			System.out.println("Leap Year");
+//		}
+		
+		
+		//Check Values lies within Quadrant of circle
+		
+//		int x,y;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter X: ");
+//		x = sc.nextInt();
+//		System.out.println("Enter y: ");
+//		y = sc.nextInt();
+//		if(x>0 && y>0)
+//		{
+//			System.out.println("1st Quadrant");
+//		}
+//		
+//		if(x>0 && y<0)
+//		{
+//			System.out.println("4th Quadrant");
+//		}
+//		if(x<0 && y>0)
+//		{
+//			System.out.println("2nd Quadrant");
+//		}
+//		if(x<0 && y<0)
+//		{
+//			System.out.println("3rd Quadrant");
+//		}
+		
+		
+		// Even and Odd Number
+		
+//		int num1;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Num1: ");
+//		num1 = sc.nextInt();
+//		
+//		if(num1%2 == 0)
+//		{
+//			System.out.println("Even Number");
+//		}else
+//		{
+//			System.out.println("Odd Number");
+//		}
+		
+		//Character Classifier (Not Completed)
+		
+//		String ch;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Char: ");
+//		ch = sc.next();
+//		
+//		int ascii = ch.charAt(0);
+//		
+//		if(ch.charAt(0) == 'A' || ch.charAt(0) == 'E' || ch.charAt(0) == 'I' || ch.charAt(0) == 'O' || ch.charAt(0) == 'U' ||ch.charAt(0) == 'a' || ch.charAt(0) == 'e' || ch.charAt(0) == 'i' || ch.charAt(0) == 'o' || ch.charAt(0) == 'u')
+//		{
+//			System.out.println("Vowels");
+//		}else
+//		{
+//			if(ascii >= 48 && ascii <= 57)
+//			{
+//				System.out.println("Digits");
+//			}
+//		
+//		}
+		
+		
+		//Grade Calculator
+		
+//		double score;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Score: ");
+//		score = sc.nextDouble();
+//		
+////		if(score instanceof double)
+////		{
+////			
+////		}
+//		if(score >= 90)
+//		{
+//			System.out.println("A");
+//		}else if(score >= 80 && score <= 89)
+//		{
+//			System.out.println("B");
+//		}else if(score >= 70 && score <= 79)
+//		{
+//			System.out.println("C");
+//		}else if(score >= 60 && score <= 69)
+//		{
+//			System.out.println("D");
+//		}else if(score <= 60)
+//		{
+//			System.out.println("E");
+//		}
+		
+		
+		//Number to Words
+		
+		int score;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Score: ");
+		score = sc.nextInt();
+		String res = "";
+		int arr[] = new int[2];
+	
+		for(int i = arr.length - 1;i>=0;i--)
+		{
+			int digit = (int)score % 10;
+			score = score/10;
+			arr[i] = digit;
+		}
+			
+		for(int j = 0;j<arr.length;j++)
+		{	
+			switch(arr[j])
+			{
+			case 0:
+				res += "Zero ";
+				break;
+			case 1:
+				res += "One ";
+				break;
+			case 2:
+				res += "Two ";
+				break;
+			case 3:
+				res += "Three ";
+				break;
+			case 4:
+				res += "Four ";
+				break;
+			case 5:
+				res += "Five ";
+				break;
+			case 6:
+				res += "Six ";
+				break;
+			case 7:
+				res += "Seven ";
+				break;
+			case 8:
+				res += "Eight ";
+				break;
+			case 9:
+				res += "Nine ";
+				break;
+			default:
+				System.out.println("Invalid");
+				break;
+			}
+			
+		}
+
+		System.out.println(res);
+	}
+}
